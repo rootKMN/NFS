79a6d6cce7c1bd5b0d35f71a8c57f7c5b83054ee
rootKMN <138158566+rootKMN@users.noreply.github.com>
2023-08-01 10:13:21 +0300

    Create nfsclient_script.sh

---
+++ nfsclient_script.sh
@@ -0,0 +1,10 @@
+#install NFS utils
+yum install nfs-utils
+
+#automount NFS
+echo "192.168.50.10:/srv/share/ /mnt nfs vers=3,proto=udp,noauto,x-systemd.automount 0 0" >> /etc/fstab
+systemctl daemon-reload
+systemctl restart remote-fs.target
+
+#Create check file
+touch /mnt/nfs/client_file_check
